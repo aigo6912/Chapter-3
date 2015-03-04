177ba1330d6f412eb89146411c7822aefae47e26
aigo6912 <aigo6912@colorado.edu>
2015-03-04 17:30:28 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
     get :home
     assert_response :success
@@ -10,5 +11,30 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :help
     assert_response :success
   end
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+end
+require 'test_helper'
 
+class StaticPagesControllerTest < ActionController::TestCase
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
 end
