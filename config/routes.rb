f48bdd47458c7d3cc9f9d66129b4615119b0db5b
aigo6912 <aigo6912@colorado.edu>
2015-03-06 01:16:58 +0000

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/...'
-
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
+  get 'static_pages/home', to: 'static_pages#home'
+  get 'static_pages/help', to: 'static_pages#help'
+  get 'static_pages/about', to: 'static_pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
